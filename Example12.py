d51ade93cbe3b274458e3a24c0c0aebef3355860
gondaliyahiren99-commits <gondaliyahiren99@gmail.com>
2026-03-31 17:40:05 +0530

    updated...

--- Example12.py
+++ Example12.py
@@ -1,15 +1,14 @@
 marks = int(input("Enter marks"))
 
-if mrks >= 0 and marks<=100:
-    if marks >= 70:
-    print("A grade")
-    elif marks >= 60:
-    print("B grade ")
-    elif marks >= 50:
-    print("C grade")
-    elif marks >= 40 :
-    print("D grade")
-    else :
-    print("Fail")
+if marks>=0 and marks<=100:
+    if marks>60:
+        print("A")
+    elif marks>50:
+        print("B")
+    elif marks>40:
+        print("C")
+    elif marks>30:
+        print("fail")
 else:
-    print("Inaal,")
+    print("invalid")
+    
